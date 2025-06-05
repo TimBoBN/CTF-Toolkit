b526d6889c42bf1dbd4b63708c165a9ce0b36138
TimBoBN <154078170+TimBoBN@users.noreply.github.com>
2025-06-05 21:23:38 +0200

    v 0.2.7
    Translation

--- cli/hasher.py
+++ cli/hasher.py
@@ -5,13 +5,10 @@ app = typer.Typer()
 
 @app.command()
 def create(text: str, algo: str = "md5"):
-    """
-    Erstellt einen Hash aus einem Text. Standard ist MD5.
-    Verfügbare Optionen: md5, sha1, sha256, sha512
-    """
+
     try:
         hash_func = getattr(hashlib, algo)
         hashed = hash_func(text.encode()).hexdigest()
         typer.echo(f"[🔒] {algo.upper()} → {hashed}")
     except AttributeError:
-        typer.echo(f"[❌] Ungültiger Algorithmus: {algo}")
+        typer.echo(f"[❌] Invalid algorithm: {algo}")
